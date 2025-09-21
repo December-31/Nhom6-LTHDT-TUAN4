9bdbfce5fa20a5f571acfbd8da9021e0bd568cf4
Trương Văn Cường <vancuong31122k6@gmail.com>
2025-09-21 10:29:57 +0700

    Update and rename Baitap9.java to Bai9.java

--- Bai9.java
+++ Bai9.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Baitap9 {
+public class Bai9 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Nhập số lượng phần tử: ");
